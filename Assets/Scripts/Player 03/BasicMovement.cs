4eec1b95469b19e53af7ac08d143349113e1e93e
salga133 <vkyleisaac.133@gmail.com>
2021-03-11 01:36:45 +0800

    new assets. added tiles on Test 03, new colliders, added water

--- Assets/Scripts/Player 03/BasicMovement.cs
+++ Assets/Scripts/Player 03/BasicMovement.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public Animator animator;
+    private float moveSpeed;
+    private float dirX;
 
     void Update()
     {
+        // Normal Walk
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + movement * Time.deltaTime * moveSpeed;
 
+        // Animation
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Magnitude", movement.magnitude);
 
-        transform.position = transform.position + movement * Time.deltaTime * 5;
+        //Run(Hold Shift)
+        if (Input.GetKey (KeyCode.LeftShift))
+            moveSpeed = 8f;
+        else
+            moveSpeed = 4f;
 
+        dirX = Input.GetAxis ("Horizontal") * moveSpeed;
+        dirX = Input.GetAxis ("Vertical") * moveSpeed;
     }
 }
 
